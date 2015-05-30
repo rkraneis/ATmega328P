308a2e9b8ea7b197256e46cfff597a299dff8afd
René Kraneis <rene.kraneis@gmail.com>
2015-05-30 12:55:33 +0200

    initial commit

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,84 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=avr-gcc
+CCC=avr-g++
+CXX=avr-g++
+FC=gfortran
+AS=avr-as
+
+# Macros
+CND_PLATFORM=AVR-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=-mmcu=atmega16
+
+# CC Compiler Flags
+CCFLAGS=-mmcu=atmega16
+CXXFLAGS=-mmcu=atmega16
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atmega328p
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atmega328p: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atmega328p ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -DF_CPU=8000000UL -D__AVR_ATmega16__ -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/atmega328p
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=AVR-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/AVR-Linux-x86
+CND_ARTIFACT_NAME_Debug=atmega328p
+CND_ARTIFACT_PATH_Debug=dist/Debug/AVR-Linux-x86/atmega328p
+CND_PACKAGE_DIR_Debug=dist/Debug/AVR-Linux-x86/package
+CND_PACKAGE_NAME_Debug=atmega328p.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/AVR-Linux-x86/package/atmega328p.tar
+# Release configuration
+CND_PLATFORM_Release=AVR-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/AVR-Linux-x86
+CND_ARTIFACT_NAME_Release=atmega328p
+CND_ARTIFACT_PATH_Release=dist/Release/AVR-Linux-x86/atmega328p
+CND_PACKAGE_DIR_Release=dist/Release/AVR-Linux-x86/package
+CND_PACKAGE_NAME_Release=atmega328p.tar
+CND_PACKAGE_PATH_Release=dist/Release/AVR-Linux-x86/package/atmega328p.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
